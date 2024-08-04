8c1bb198e261d1b622b2e234bce9e6419fdbb8bf
hieulm <vunguyenn024@gmail.com>
2024-08-04 19:40:16 +0700

    [Fe-Admin - Banner] code upload anh

--- API/Admin/Model/Banner.cs
+++ API/Admin/Model/Banner.cs
@@ -5,15 +5,11 @@ namespace Admin.Model
     public class Banner
     {
         public int Id { get; set; }
-
-        [Required(ErrorMessage = "Hình ảnh chưa được nhập")]
         public string Image { get; set; }
 
         [Required(ErrorMessage = "Tiêu đề chưa được nhập.")]
-        [StringLength(30, ErrorMessage = "Không nhập quá 300 ký tự.")]
+        [StringLength(300, ErrorMessage = "Không nhập quá 300 ký tự.")]
         public string Title { get; set; }
-
-        [Required(ErrorMessage = "Trạng thái chưa được nhập")]
         [Range(0, 1, ErrorMessage = "Trạng thái")]
         public int Status { get; set; }
     }
