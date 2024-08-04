731cda4d17b9a94fa1f01c1d8caa902dd7997590
hieulm <vunguyenn024@gmail.com>
2024-08-04 18:04:14 +0700

    [FE-Admin-Banner]

--- API/Admin/Model/Banner.cs
+++ API/Admin/Model/Banner.cs
@@ -1,10 +1,20 @@
-﻿namespace Admin.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Admin.Model
 {
     public class Banner
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Hình ảnh chưa được nhập")]
         public string Image { get; set; }
+
+        [Required(ErrorMessage = "Tiêu đề chưa được nhập.")]
+        [StringLength(30, ErrorMessage = "Không nhập quá 300 ký tự.")]
         public string Title { get; set; }
-        public byte Status { get; set; }
+
+        [Required(ErrorMessage = "Trạng thái chưa được nhập")]
+        [Range(0, 1, ErrorMessage = "Trạng thái")]
+        public int Status { get; set; }
     }
 }
