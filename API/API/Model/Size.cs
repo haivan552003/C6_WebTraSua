9a2b89e9caf0b23f9bcdbb59d736167f4e30aa5e
hoangthinh1512 <thinhlhpc07113@fpt.edu.vn>
2024-08-08 15:54:12 +0700

    thinhlh-bat-loi

--- API/API/Model/Size.cs
+++ API/API/Model/Size.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -10,8 +9,15 @@ namespace API.Model
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int SizeID { get; set; }
+
+        [Required(ErrorMessage = "Tên không được bỏ trống.")]
+        [StringLength(100, ErrorMessage = "Độ dài tên kích thước không thể lớn hơn 100 ký tự.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Mã sản phẩm không được bỏ trống.")]
         public int ProductID { get; set; }
+
+        [ForeignKey("ProductID")]
         public Product Product { get; set; }
     }
 }
