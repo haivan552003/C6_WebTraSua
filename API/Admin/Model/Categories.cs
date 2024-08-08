cc359cc0ee1238425f2a3a3fb9e884bf9c12c844
hoangthinh1512 <thinhlhpc07113@fpt.edu.vn>
2024-08-08 21:46:55 +0700

    thinh-batloi-tk

--- API/Admin/Model/Categories.cs
+++ API/Admin/Model/Categories.cs
@@ -10,8 +10,17 @@ namespace Admin.Model
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 
         public int CateID { get; set; }
+
+        [Required(ErrorMessage = "Tên không được bỏ trống.")]
+        [StringLength(100, ErrorMessage = "Độ dài tên không thể lớn hơn 100 ký tự.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Hình ảnh không được bỏ trống.")]
+        [StringLength(500, ErrorMessage = "Độ dài hình ảnh không thể lớn hơn 500 ký tự.")]
         public string Image { get; set; }
+
+        [Required(ErrorMessage = "Trạng thái không được bỏ trống.")]
+        [Range(0, 255, ErrorMessage = "Trạng thái phải nằm trong khoảng từ 0 đến 255.")]
         public byte Status { get; set; }
 
         public ICollection<Product> Products { get; set; }
