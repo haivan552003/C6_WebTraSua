51b6ae41ab38d662662055692b38887671362925
VanLTH <haivan5.5.2003@gmail.com>
2024-08-08 14:29:14 +0700

    fix các lỗi sinh ra do chỉnh csdl

---
+++ API/WebTraSua/Model/UserLogin.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebTraSua.Model
+{
+    public class UserLogin
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập tên tài khoản")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+        public string PassWord { get; set; }
+        public int RoleID { get; set; }
+    }
+}
---
+++ API/WebTraSua/Model/UserRegister.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebTraSua.Model
+{
+    public class UserRegister
+    {
+        [Required(ErrorMessage = "Vui lòng nhập tên")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập tên tài khoản")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+        public string PassWord { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập lại mật khẩu")]
+        [Compare("PassWord", ErrorMessage = "Mật khẩu không khớp")]
+        public string RePass { get; set; }
+        public int RoleID { get; set; }
+    }
+}
