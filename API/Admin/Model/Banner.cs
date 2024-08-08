8814e3d555c5bde43f7c1e3f20314f117d26e8e3
hoangthinh1512 <thinhlhpc07113@fpt.edu.vn>
2024-08-08 21:46:55 +0700

    thinh-batloi-tk

--- API/Admin/Model/Banner.cs
+++ API/Admin/Model/Banner.cs
@@ -1,10 +1,22 @@
-﻿namespace Admin.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Admin.Model
 {
     public class Banner
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Hình ảnh không được bỏ trống.")]
+        [StringLength(500, ErrorMessage = "Độ dài hình ảnh không thể lớn hơn 500 ký tự.")]
         public string Image { get; set; }
+
+        [Required(ErrorMessage = "Tiêu đề không được bỏ trống.")]
+        [StringLength(100, ErrorMessage = "Độ dài tiêu đề không thể lớn hơn 100 ký tự.")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Trạng thái không được bỏ trống.")]
+        [Range(0, 255, ErrorMessage = "Trạng thái phải nằm trong khoảng từ 0 đến 255.")]
         public byte Status { get; set; }
     }
 }
--- API/Admin/Model/Size.cs
+++ API/Admin/Model/Size.cs
@@ -10,8 +10,15 @@ namespace Admin.Model
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int SizeID { get; set; }
+
+        [Required(ErrorMessage = "Tên không được bỏ trống.")]
+        [StringLength(100, ErrorMessage = "Độ dài tên kích thước không thể lớn hơn 100 ký tự.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Mã sản phẩm không được bỏ trống.")]
         public int ProductID { get; set; }
+
+        [ForeignKey("ProductID")]
         public Product Product { get; set; }
     }
 }
